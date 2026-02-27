4f728575dec850a08649e96ed9dec3df04a2777b
oscar <oscarollie366@gmail.com>
2026-02-27 18:40:55 +0300

    Update main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { SocketProvider } from "./context/SocketContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <SocketProvider>
+  <React.StrictMode>
     <App />
-  </SocketProvider>,
+  </React.StrictMode>,
 );
-
-{
-  /* <React.StrictMode>
-    <App />
-  </React.StrictMode>, */
-}
